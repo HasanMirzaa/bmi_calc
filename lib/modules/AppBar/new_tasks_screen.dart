c006a4941bfdcb71c43591020eb71099f053d543
Hasan <hasanboulad@yahoo.com>
2022-06-27 14:43:37 +0300

    Done

--- lib/modules/AppBar/new_tasks_screen.dart
+++ lib/modules/AppBar/new_tasks_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/components/components.dart';
+import '../../shared/components/constants.dart';
+
 class NewTasksScreen extends StatelessWidget {
   const NewTasksScreen({Key? key}) : super(key: key);
 
+  //const NewTasksScreen({Key? key,  required this.tasks}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'New Tasks',
-        style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-      ),
-    );
+    return ListView.separated(
+        itemBuilder: ((context, index) => buildTaskItem()),
+        separatorBuilder: (context, index) => Padding(
+              padding: const EdgeInsetsDirectional.only(start: 20.0),
+              child: Container(
+                width: double.infinity,
+                height: 1.0,
+                color: Colors.grey[300],
+              ),
+            ),
+        itemCount: tasks.length);
   }
 }
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -51,3 +51,31 @@ Widget defaultTextFormField({
               : null,
           border: const OutlineInputBorder()),
     );
+
+Widget buildTaskItem() => Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          const CircleAvatar(
+            radius: 40.0,
+            child: Text('02:00 pm'),
+          ),
+          const SizedBox(
+            width: 20.0,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text(
+                'Task Title',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '2 april, 2022',
+                style: TextStyle(color: Colors.grey),
+              )
+            ],
+          )
+        ],
+      ),
+    );
